43a9cd628cf51dfa26fb69e194760b7d9fa38970
Snago1 <snagogen@gmail.com>
2025-02-18 23:13:32 +0300

    Описание изменений

--- Documents/react-study/project2/project3/src/App.jsx
+++ Documents/react-study/project2/project3/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
+import "./index.css";
+
+const KEY = "0aeadb9efb1f41ef9c9195031251802";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="widget-container">
+        <div className="weather-card-container">
+          <h1 className="app-title">Weather Widget</h1>
+          <div className="search-container">
+            <input
+              type="text"
+              placeholder="Enter city name"
+              className="search-input"
+            />
+          </div>
+        </div>
+        <div className="weather-card">
+          <h2>Moscow, Russia</h2>
+          <img src="" alt="icon" className="weather-icon" />
+          <p className="temperature">11°C</p>
+          <p className="condition">rainy</p>
+          <div className="weather-details">
+            <p>Humidity: 20%</p>
+            <p>Wind: 22 km/h</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React1</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
