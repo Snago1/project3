efb531e1fabeae70a6e5c1c6602944f91fa183a3
Snago1 <snagogen@gmail.com>
2025-02-19 00:18:26 +0300

    some changes

--- Documents/react-study/project2/project3/src/App.jsx
+++ Documents/react-study/project2/project3/src/App.jsx
@@ -4,6 +4,20 @@ import "./index.css";
 const KEY = "0aeadb9efb1f41ef9c9195031251802";
 
 function App() {
+  const [city, setCity] = useState("London");
+  const [weaterData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}`
+      );
+      const data = await res.json();
+      setWeatherData(data);
+    }
+    getData();
+  }, []);
+  console.log(weaterData);
   return (
     <div className="app">
       <div className="widget-container">
@@ -18,7 +32,9 @@ function App() {
           </div>
         </div>
         <div className="weather-card">
-          <h2>Moscow, Russia</h2>
+          <h2>
+            {`${weaterData?.location.name}, ${weaterData?.location.country}`}
+          </h2>
           <img src="" alt="icon" className="weather-icon" />
           <p className="temperature">11°C</p>
           <p className="condition">rainy</p>
