f37c74a25ccb1f8e13bb3a416b9ae758210ea2ba
Геннадий Снагощенко <gennadijsnagosenko@Mac-mini-Gennadij.local>
2025-04-01 21:58:36 +0300

    before useReducer

--- Documents/react_course/course3/course3/src/App.jsx
+++ Documents/react_course/course3/course3/src/App.jsx
@@ -6,25 +6,44 @@ import Header from "./components/Header/Header";
 import JournalList from "./components/JournalList/JournalList";
 import JournalAdd from "./components/JournalAdd/JournalAdd";
 import JournalForm from "./components/JournalForm/JournalForm";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const INITIAL_DATA = [
-  // {
-  //   id: 1,
-  //   title: "Подготовка к обновлению курсов",
-  //   date: new Date(),
-  //   post: "Сегодня провёл весь день за...",
-  // },
-  // {
-  //   id: 2,
-  //   title: "Поход в годы",
-  //   date: new Date(),
-  //   post: "Думал, что очень много времени...",
-  // },
-];
+// const INITIAL_DATA = [
+//   {
+//     id: 1,
+//     title: "Подготовка к обновлению курсов",
+//     date: new Date(),
+//     post: "Сегодня провёл весь день за...",
+//   },
+//   {
+//     id: 2,
+//     title: "Поход в годы",
+//     date: new Date(),
+//     post: "Думал, что очень много времени...",
+//   },
+// ];
+
 
 const App = () => {
-  const [items, setItems] = useState(INITIAL_DATA);
+  const [items, setItems] = useState([]);
+
+  useEffect(()=>{
+    const data = JSON.parse(localStorage.getItem("data"));
+    if(data){
+      setItems(data.map(item =>({
+        ...item, 
+        date: new Date(item.date)
+      })));
+    }
+  },[])
+
+  useEffect(()=>{
+    if(items.length){
+      console.log('Post');
+      localStorage.setItem('data', JSON.stringify(items));
+    }
+  },[items])
+  
 
   const addItem = (item) => {
     setItems((oldItems) => [
--- Documents/react_course/course3/course3/src/components/JournalForm/JournalForm.jsx
+++ Documents/react_course/course3/course3/src/components/JournalForm/JournalForm.jsx
@@ -1,14 +1,28 @@
 import Button from "../Button/Button";
 import styles from "./JournalForm.module.css";
-import { useState } from "react";
-// import cn from "classnames";
+import { useEffect, useState } from "react";
+import cn from "classnames";
+
+const INITIAL_STATE = {
+  title: true,
+  post: true,
+  date: true,
+};
 
 const JournalForm = ({ onSubmit }) => {
-  const [formValidState, setFormValidState] = useState({
-    title: true,
-    post: true,
-    date: true,
-  });
+  const [formValidState, setFormValidState] = useState(INITIAL_STATE);
+
+  useEffect(() => {
+    let timerID;
+    if (!formValidState.date || !formValidState.title || !formValidState.post) {
+      timerID = setTimeout(() => {
+        setFormValidState(INITIAL_STATE);
+      }, 2000);
+    }
+    return () => {
+      clearTimeout(timerID);
+    };
+  }, [formValidState]);
 
   const addJournalItem = (e) => {
     e.preventDefault();
@@ -40,29 +54,52 @@ const JournalForm = ({ onSubmit }) => {
   };
   return (
     <form className={styles["journal-form"]} onSubmit={addJournalItem}>
-      <input
-        type="text"
-        name="title"
-        className={`${styles["input"]} ${
-          formValidState.title ? "" : styles["invalid"]
-        }`}
-      />
-      <input
-        type="date"
-        name="date"
-        className={`${styles["input"]} ${
-          formValidState.date ? "" : styles["invalid"]
-        }`}
-      />
-      <input type="text" name="post" />
+      <div>
+        <input
+          type="text"
+          name="title"
+          className={cn(styles["input-title"], {
+            [styles["invalid"]]: !formValidState.title,
+          })}
+        />
+      </div>
+      <div className={styles["form-row"]}>
+        <label htmlFor="date" className={styles["form-label"]}>
+          <img src="/calendar.svg" alt="Календарь" />
+          <span>Дата</span>
+        </label>
+        <input
+          type="date"
+          name="date"
+          id="date"
+          className={cn(styles["input"], {
+            [styles["invalid"]]: !formValidState.date,
+          })}
+        />
+      </div>
+
+      <div className={styles["form-row"]}>
+        <label htmlFor="tag" className={styles["form-label"]}>
+          <img src="/folder.svg" alt="Папки" />
+          <span>Метки</span>
+        </label>
+
+        <input
+          type="text"
+          id="tag"
+          name="tag"
+          className={cn(styles["input"])}
+        />
+      </div>
+
       <textarea
         name="post"
-        id=""
+        id="post"
         cols="30"
         rows="10"
-        className={`${styles["input"]} ${
-          formValidState.post ? "" : styles["invalid"]
-        }`}
+        className={cn(styles["input"], {
+          [styles["invalid"]]: !formValidState.post,
+        })}
       ></textarea>
       <Button text="Сохранить" />
     </form>
